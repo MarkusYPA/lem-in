823ca544eba9b3ad749ea1ea38e44a18b6259bcf
Markus Amberla <markus.amberla@gmail.com>
2024-12-18 17:02:09 +0200

    attempt to speed it up

--- ants.go
+++ ants.go
@@ -18,27 +18,37 @@ func makeAnts(optimals [][]route, n int) [][]ant {
 }
 
 // assignRoutes gives each ant a route to follow
-func assignRoutes(optimals [][]route, sAnts *[][]ant) {
-	for iRs, routes := range optimals {
+func assignRoutes(optimals [][]route, setsOfAnts *[][]ant) {
+	//wgCombos := sync.WaitGroup{}
+	for i, routeCombo := range optimals {
+		//wgCombos.Add(1)
+		//go func() {
 		// how many ants on each route in this combo
-		onRoutes := make([]int, len(routes))
-
+		onRoutes := make([]int, len(routeCombo))
+		//wgAnts := sync.WaitGroup{}
 		// loop over the set of ants pertaining to the combo of routes
-		for i := 0; i < len((*sAnts)[iRs]); i++ {
-
+		for j := 0; j < len((*setsOfAnts)[i]); j++ {
+			//wgAnts.Add(1)
+			//go func() {
 			// find the shortest route for this ant (length = route length + ants already taking it)
 			shortest := 0
-			shortD := len(routes[0]) + onRoutes[0]
-			for j := 0; j < len(routes); j++ {
-				if len(routes[j])+onRoutes[j] < shortD {
-					shortest = j
-					shortD = len(routes[j]) + onRoutes[j]
+			shortD := len(routeCombo[0]) + onRoutes[0]
+			for k := 0; k < len(routeCombo); k++ {
+				if len(routeCombo[k])+onRoutes[k] < shortD {
+					shortest = k
+					shortD = len(routeCombo[k]) + onRoutes[k]
 				}
 			}
-			(*sAnts)[iRs][i].Route = routes[shortest]
+			(*setsOfAnts)[i][j].Route = routeCombo[shortest]
 			onRoutes[shortest]++
+			//wgAnts.Done()
+			//}()
 		}
+		//wgAnts.Wait()
+		//wgCombos.Done()
+		//}()
 	}
+	//wgCombos.Wait()
 }
 
 // getEndInd returns the index of the "end" room
@@ -52,11 +62,11 @@ func getEndInd(rs []room) int {
 }
 
 // nextRoom finds the next room on an ant's route
-func nextRoom(rms []room, curr room, a ant) room {
-	var next room
+func nextRoom(rms *[]room, curr room, a ant) *room {
+	var next *room
 	for i, roomName := range a.Route {
 		if roomName == curr.Name && i < len(a.Route)-1 {
-			next = rms[findRoom(rms, a.Route[i+1])]
+			next = &(*rms)[findRoom((*rms), a.Route[i+1])]
 		}
 	}
 	return next
@@ -64,30 +74,31 @@ func nextRoom(rms []room, curr room, a ant) room {
 
 // nextIsOk returns true if the next room has space and
 // the route to it hasn't been used on this turn already
-func nextIsOk(a ant, rooms []room, usedLinks [][2]string) bool {
-	var curr room
-	for _, rm := range rooms {
+func nextIsOk(a ant, rooms *[]room, usedLinks [][2]string) (bool, *room, *room) {
+	var curr *room
+	var next *room
+	for i, rm := range *rooms {
 		for _, occ := range rm.Occupants {
 			if occ == a.Name {
-				curr = rm
+				curr = &(*rooms)[i]
 			}
 		}
 	}
 	if curr.Role == "end" {
-		return false
+		return false, curr, next
 	}
 
-	next := nextRoom(rooms, curr, a)
+	next = nextRoom(rooms, *curr, a)
 
 	// false if this link was already used on this turn
 	for _, link := range usedLinks {
 		if [2]string{curr.Name, next.Name} == link {
-			return false
+			return false, curr, next
 		}
 	}
 
 	// true if next room is empty or the end
-	return len(next.Occupants) < 1 || next.Role == "end"
+	return len(next.Occupants) < 1 || next.Role == "end", curr, next
 }
 
 // removeFromRoom removes ant a from the lists of occupants of all rooms and returns the current and next rooms
@@ -106,8 +117,22 @@ func removeFromRoom(rms []room, a ant) (room, room, []room) {
 		}
 		roomsOut = append(roomsOut, nuRm)
 	}
-	next := nextRoom(rms, curr, a)
-	return curr, next, roomsOut
+	next := nextRoom(&rms, curr, a)
+	return curr, *next, roomsOut
+}
+
+func removeAntFromRoom(thisRoom *room, a ant) {
+	//before := time.Now()
+	newOccupants := []int{}
+	for _, oc := range thisRoom.Occupants {
+		//fmt.Println(time.Since(before), "0")
+		if oc != a.Name {
+			newOccupants = append(newOccupants, oc)
+		}
+		//fmt.Println(time.Since(before), "1")
+		//before = time.Now()
+	}
+	thisRoom.Occupants = newOccupants
 }
 
 // addToRoom adds an ant to the list of occupants in a room
@@ -130,18 +155,14 @@ func moveAnts(rms *[]room, ants []ant) []string {
 
 		// try to move each ant
 		for i := 0; i < len(ants); i++ {
-
-			if nextIsOk(ants[i], *rms, linksUsed) {
-				// remove ant from any and all rooms, and mark this link as used
-				var curr, next room
-				curr, next, *rms = removeFromRoom(*rms, ants[i])
-				linksUsed = append(linksUsed, [2]string{curr.Name, next.Name})
-
-				// put ant to next room
-				addToRoom(rms, &next, ants[i])
+			NextOk, currentRoom, nextRoom := nextIsOk(ants[i], rms, linksUsed)
+			if NextOk {
+				removeAntFromRoom(currentRoom, ants[i])
+				linksUsed = append(linksUsed, [2]string{currentRoom.Name, nextRoom.Name}) // mark this link as used
+				addToRoom(rms, nextRoom, ants[i])
 
 				// add move to current turn
-				moves += "L" + strconv.Itoa(ants[i].Name) + "-" + next.Name + " "
+				moves += "L" + strconv.Itoa(ants[i].Name) + "-" + nextRoom.Name + " "
 			}
 		}
 
--- lemin.go
+++ lemin.go
@@ -68,8 +68,9 @@ func main() {
 	// find all routes connecting "start" to "end" and all unique combinations of non-crossing routes
 	var routes []route
 	findRoutes(rooms[getStartInd(rooms)], route{}, &routes, &rooms)
+	//fmt.Println("Routes", time.Now().Format("05.00"))
 	sortRoutes(&routes)
-	sepRoutes := getSepRoutes(routes)
+	separateRoutes := getSepRoutes(routes)
 
 	/*
 		Two crossing routes work effectively as one single route because of the
@@ -81,15 +82,23 @@ func main() {
 		- A combination with the lowest average route length (possibly for a medium amount of ants)
 	*/
 
-	optimals := reduceOptimals([][]route{shortCombo(sepRoutes, routes), longCombo(sepRoutes), bestScoreCombo(sepRoutes)})
+	optimals := reduceOptimals([][]route{shortCombo(separateRoutes, routes), longCombo(separateRoutes), bestScoreCombo(separateRoutes)})
+	//fmt.Println(time.Now().Format("05.00"), "Optimals")
 	setsOfAnts := makeAnts(optimals, nAnts)
+	//fmt.Println(time.Now().Format("05.00"), "Ants made")
 	assignRoutes(optimals, &setsOfAnts)
+	//fmt.Println(time.Now().Format("05.00"), "Assigned")
 	_, optI := bestSolution(optimals, setsOfAnts)
+	//fmt.Println(time.Now().Format("05.00"), "Best")
 	populateStart(&rooms, setsOfAnts[optI])
+	//fmt.Println(time.Now().Format("05.00"), "Populated")
 
 	// Move ants and save the moves
 	turns := moveAnts(&rooms, setsOfAnts[optI])
+	//fmt.Println(time.Now().Format("05.00"), "Moved")
 
 	// Print out the file contents and the moves
 	printSolution(string(in), turns)
+	//fmt.Println()
+	//fmt.Println(len(turns), "turns")
 }
